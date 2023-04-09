2186ca6d9f8bd866e7ea133886f1fee3837fe086
finddreams <finddreams@gmail.com>
2023-04-09 14:05:32 +0800

    使用最新的1.5.0预览版 测试性能，结果滑动列表和1.2正式版差不了多少；

--- app/src/main/java/com/zj/compose/fps/ui/page/list/widget/HomeBannerComposable.kt
+++ app/src/main/java/com/zj/compose/fps/ui/page/list/widget/HomeBannerComposable.kt
@@ -1,33 +1,32 @@
 package com.zj.compose.fps.ui.page.list.widget
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.PagerState
 import com.zj.compose.fps.ui.page.list.BannerList
 
 
-@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun HomeBannerComposable(item: BannerList) {
     HomeBannerSection(item)
 }
 
-@ExperimentalPagerApi
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeBannerSection(data: BannerList) {
     val bannerState = remember {
         PagerState(data.list.size)
     }
-    HorizontalPager(state = bannerState, modifier = Modifier.fillMaxWidth()) {
-        val cover = data.list[this.currentPage].cover
+    HorizontalPager(state = bannerState, pageCount = data.list.size,) {
+        val cover = data.list[it].cover
         Box {
             NetworkImage(
                 url = cover,
