08be658f31bcd3e365261cc7490f4db58cca1ffc
xiang <xiang>
2024-06-07 10:56:55 +0800

    升级到1.7.0 R8 模式，滑动很流畅；

--- app/src/main/java/com/zj/compose/fps/ui/page/list/widget/HomeBannerComposable.kt
+++ app/src/main/java/com/zj/compose/fps/ui/page/list/widget/HomeBannerComposable.kt
@@ -1,14 +1,12 @@
 package com.zj.compose.fps.ui.page.list.widget
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.zj.compose.fps.ui.page.list.BannerList
@@ -19,13 +17,10 @@ fun HomeBannerComposable(item: BannerList) {
     HomeBannerSection(item)
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeBannerSection(data: BannerList) {
-    val bannerState = remember {
-        PagerState(data.list.size)
-    }
-    HorizontalPager(state = bannerState, pageCount = data.list.size,) {
+    val bannerState = rememberPagerState { data.list.size }
+    HorizontalPager(state = bannerState) {
         val cover = data.list[it].cover
         Box {
             NetworkImage(
